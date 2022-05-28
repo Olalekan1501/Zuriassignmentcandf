160e3b48842dec1383a479fad9c79decbc4c3528
Olalekan Ayorinde <84253380+Olalekan1501@users.noreply.github.com>
2022-05-28 23:27:13 +0100

    Add files via upload

---
+++ main classes.py
@@ -0,0 +1,30 @@
+class Student():
+#lassignment) Skeleton class. Add your code here
+  def __init__(self, name, age, tracks, score):
+    self.name = name
+    self.age age self.tracks tracks
+    self.score score
+    pass
+
+  def change_name(self, new_name):
+    self.name= new_name
+
+  def change_age(self,new_age):
+    self.age = new age
+
+  def add_track(self,new_track):
+    self.tracks = new_track
+
+  def get_score(self):
+    if is instance(self.age,int):
+      print(self.name, self.age,self.tracks, self.score)
+    else:
+      print("Age must be int. please try again")
+
+Bob = Student (name="Bob", age=26, tracks=["FE", "BE"],score=20.98)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.add_track("UI/UX")
+Bob.change_age(34)
+Bob.get_score()
